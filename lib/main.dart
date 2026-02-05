bbb77b67c068251a5f47bfabc4a205cbe1c24b8a
mujica1301 <andresemiliomujica2006@gmail.com>
2026-02-04 22:20:30 -0400

    Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,23 @@
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/material.dart'; 
+import '../screens/game_screen.dart'; 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MemoryGameApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MemoryGameApp extends StatelessWidget {
+  const MemoryGameApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
+    return MaterialApp(
+      title: 'Flutter Memoria',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
-      ),
+      home: const GameScreen(), 
     );
   }
 }
